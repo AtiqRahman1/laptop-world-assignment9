d01cb07b2cb379c139c5440dc0b6278f16732af2
AtiqRahman1 <atiq94.ar@gmail.com>
2022-04-05 00:28:09 +0600

    blogs complete

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div>
-            <h2>Questions will write down here</h2>
+        <div className='blog-text'>
+            <h2>Question: What is Context API?</h2>
+            <p>Answer: The context API is the way for a react app to generate global variables which can be passed around. The variation to prop drilling or moving props from grandparent to child to parent and so on. React.creatContext() returns a consumer and a provider. Provider is a component that provides the state to its children. It holds the store and be the parent of all components that may need that store. Consumer requires a function as a child. The function receives the context value and returns a react node.</p>
+            <h2>Question: What is semantic tag? explain with example.</h2>
+            <p>Answer: Semantic tag makes the code easier to write and understand for the developer as well as instructs the browser on how to treat them. When a page is well designed visually, it is easy to identify the different parts of web page. By using semantic element, google understands which is the most important content. It gives to the browser an explicit instruction to prioritize content. The example of semantic elements are header,main,nav,section,article,footer etc.</p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                 <div className='image'>
                     <img src={image} alt="" />
                 </div>
-                <div className='text'>
+                <div className='text-area'>
                     <h1>Your dream !</h1>
                     <h1>Your laptop!!</h1>
                     <p>Create on-the-go with a convertible laptop designed to move with you. The power of a 360 degree hinge combined with the simultaneous use of touch and pen make for vibrant, accurate creations. With up a long battery life and peace of mind features, you can create from day to night with ease.</p>
