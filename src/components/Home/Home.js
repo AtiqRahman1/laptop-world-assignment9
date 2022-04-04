aceb63bca73e07c0ebd3619ec658437188c6398d
AtiqRahman1 <atiq94.ar@gmail.com>
2022-04-04 14:38:08 +0600

    customer reviews done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,21 @@ import image from '../image/HP-laptop.jpg';
 
 const Home = () => {
     return (
-        <div className='container'>
-            <div className='image'>
-                <img src={image} alt="" />
-            </div>
-            <div className='text'>
-                <h1>Your dream !</h1>
-                <h1>Your laptop!!</h1>
-                <p>Create on-the-go with a convertible laptop designed to move with you. The power of a 360 degree hinge combined with the simultaneous use of touch and pen make for vibrant, accurate creations. With up a long battery life and peace of mind features, you can create from day to night with ease.</p>
-            </div>
+        <div>
+            <section className='container'>
+                <div className='image'>
+                    <img src={image} alt="" />
+                </div>
+                <div className='text'>
+                    <h1>Your dream !</h1>
+                    <h1>Your laptop!!</h1>
+                    <p>Create on-the-go with a convertible laptop designed to move with you. The power of a 360 degree hinge combined with the simultaneous use of touch and pen make for vibrant, accurate creations. With up a long battery life and peace of mind features, you can create from day to night with ease.</p>
+                </div>
+            </section>
+            <section className='customer-container'>
+                <h1>Customer Reviews</h1>
+                <button className='reviews-btn'>See All Reviews</button>
+            </section>
         </div>
 
     );
